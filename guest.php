5e9f37df0777dda5d34277d56e94a4b429df0ea9
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-01-26 14:30:20 +0000

    guest

--- guest.php
+++ guest.php
@@ -33,7 +33,7 @@ if ($_GET['action'] == "edit" || $_GET['action'] == "view" || $_GET['action'] ==
 
     $wedding->execute();
     $wedding->store_result();
-    $wedding->bind_result($wedding_id, $wedding_name, $wedding_email, $wedding_phone, $wedding_contact_name);
+    $wedding->bind_result($wedding_id, $wedding_name, $wedding_date, $wedding_email, $wedding_phone, $wedding_contact_name);
     $wedding->fetch();
     $wedding->close();
 } else {
