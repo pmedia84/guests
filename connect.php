46a346bfafdeb7b7001f187e034d9c0f552cb006
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-01-27 09:19:47 +0000

    connect

--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'alexemma_admin';
-    $DATABASE_PASS = 'Krb833908';
-    $DATABASE_NAME = 'alexemma_admin';
-$db = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME,);
