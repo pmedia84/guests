4ac85cda572066c8471dfbab543e0bd1c1ec1479
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-07-25 20:20:48 +0100

    edits

--- profile.php
+++ profile.php
@@ -12,10 +12,10 @@ $cms_name = "";
 $user_id = $_SESSION['user_id'];
 
 //guest variable, only required for edit and view actions
-    // load this users details
-    $user_details_query = $db->query('SELECT users.user_id, users.guest_id, guest_list.guest_fname, guest_list.guest_sname, guest_list.guest_email, guest_list.guest_address, guest_list.guest_postcode, guest_list.guest_dietery  FROM users LEFT JOIN guest_list ON guest_list.guest_id=users.guest_id WHERE users.user_id =' . $user->user_id());
-    $user_details_result = $user_details_query->fetch_assoc();
-    //define guest group id
+// load this users details
+$user_details_query = $db->query('SELECT users.user_id, users.guest_id, guest_list.guest_fname, guest_list.guest_sname, guest_list.guest_email, guest_list.guest_address, guest_list.guest_postcode, guest_list.guest_dietery  FROM users LEFT JOIN guest_list ON guest_list.guest_id=users.guest_id WHERE users.user_id =' . $user->user_id());
+$user_details_result = $user_details_query->fetch_assoc();
+//define guest group id
 
 ?>
 <!-- Meta Tags For Each Page -->
@@ -44,29 +44,29 @@ $user_id = $_SESSION['user_id'];
                 <a href="profile" class="breadcrumb">My Profile</a>
                 <?php if (array_key_exists('action', $_GET) && $_GET['action'] == "edit") : ?>
                     / Edit My Profile
-                <?php endif;?>
+                <?php endif; ?>
 
 
 
             </div>
             <div class="main-cards">
-                <?php if (array_key_exists('action', $_GET) && $_GET['action'] == "edit") :?>
+                <?php if (array_key_exists('action', $_GET) && $_GET['action'] == "edit") : ?>
                     <h1>Edit My Profile</h1>
                 <?php endif; ?>
-                <?php if(!array_key_exists('action', $_GET)) : ?>
+                <?php if (!array_key_exists('action', $_GET)) : ?>
                     <h1><i class="fa-solid fa-address-book"></i> My Profile</h1>
                     <div class="std-card">
                         <h2>Contact Details</h2>
-                        <p><strong>First Name: </strong><?=$user_details_result['guest_fname'];?></p>
-                        <p><strong>Last Name: </strong><?=$user_details_result['guest_sname'];?></p>
-                        <p><strong>eMail: </strong><?=$user_details_result['guest_email'];?></p>
+                        <p><strong>First Name: </strong><?= $user_details_result['guest_fname']; ?></p>
+                        <p><strong>Last Name: </strong><?= $user_details_result['guest_sname']; ?></p>
+                        <p><strong>Email: </strong><?= $user_details_result['guest_email']; ?></p>
                         <h2>Address:</h2>
                         <address>
-                            <?=$user_details_result['guest_address'];?><br>
-                            <?=$user_details_result['guest_postcode'];?><br>
+                            <?= $user_details_result['guest_address']; ?><br>
+                            <?= $user_details_result['guest_postcode']; ?><br>
                         </address>
                         <h2>Dietary Requirements</h2>
-                        <?=$user_details_result['guest_dietery'];?><br>
+                        <?= $user_details_result['guest_dietery']; ?><br>
                         <div class="card-actions">
                             <a href="profile?action=edit" class="btn-primary">Edit My Profile <i class="fa-solid fa-pen-to-square"></i></a>
                         </div>
@@ -74,41 +74,41 @@ $user_id = $_SESSION['user_id'];
                 <?php endif; ?>
 
                 <?php if (array_key_exists('action', $_GET) && $_GET['action'] == "edit") : ?>
-                    <?php if (($user_details_query->num_rows) > 0):?>
-                        
+                    <?php if (($user_details_query->num_rows) > 0) : ?>
+
                         <div class="std-card">
-                        <h2>Contact Details</h2>
+                            <h2>Contact Details</h2>
                             <form class="form-card" id="edit_profile" action="scripts/guest.script.php" method="POST" enctype="multipart/form-data">
                                 <div class="form-input-wrapper">
                                     <label for="guest_fname"><strong>First Name</strong></label>
                                     <!-- input -->
-                                    <input class="text-input input" type="text" name="guest_fname" id="guest_fname" placeholder="Guest First Name" required="" maxlength="45" value="<?= $user_details_result['guest_fname'];?>">
+                                    <input class="text-input input" type="text" name="guest_fname" id="guest_fname" placeholder="Guest First Name" required="" maxlength="45" value="<?= $user_details_result['guest_fname']; ?>">
                                 </div>
                                 <div class="form-input-wrapper">
                                     <label for="guest_sname"><strong>Surname</strong></label>
                                     <!-- input -->
-                                    <input class="text-input input" type="text" name="guest_sname" id="guest_sname" placeholder="Guest Surname" required="" maxlength="45" value="<?= $user_details_result['guest_sname'];?>">
+                                    <input class="text-input input" type="text" name="guest_sname" id="guest_sname" placeholder="Guest Surname" required="" maxlength="45" value="<?= $user_details_result['guest_sname']; ?>">
                                 </div>
                                 <div class="form-input-wrapper my-2">
-                                        <label for="guest_email"><strong>Email Address</strong></label>
-                                        <p class="form-hint-small">Changing this will also change the email address you use to login to this guest area.</p>
-                                        <p class="form-hint-small">You will also be required to change your password next time you login.</p>
-                                        <input class="text-input input" type="text" id="guest_email" name="guest_email" placeholder="Email Address" value="<?= $user_details_result['guest_email'];?>">
-                                    </div>
+                                    <label for="guest_email"><strong>Email Address</strong></label>
+                                    <p class="form-hint-small">Changing this will also change the email address you use to login to this guest area.</p>
+                                    <p class="form-hint-small">You will also be required to change your password next time you login.</p>
+                                    <input class="text-input input" type="text" id="guest_email" name="guest_email" placeholder="Email Address" value="<?= $user_details_result['guest_email']; ?>">
+                                </div>
 
-                                    <div class="form-input-wrapper my-2">
-                                        <label for="guest_address"><strong>Address</strong></label>
-                                        <textarea name="guest_address" id="guest_address"><?= $user_details_result['guest_address']; ?></textarea>
-                                    </div>
+                                <div class="form-input-wrapper my-2">
+                                    <label for="guest_address"><strong>Address</strong></label>
+                                    <textarea name="guest_address" id="guest_address"><?= $user_details_result['guest_address']; ?></textarea>
+                                </div>
 
-                                    <div class="form-input-wrapper my-2">
-                                        <label for="guest_postcode"><strong>Postcode</strong></label>
-                                        <input class="text-input input" type="text" id="guest_postcode" name="guest_postcode" placeholder="Postcode" value="<?= $user_details_result['guest_postcode'];?>">
-                                    </div>
-                                    <div class="form-input-wrapper my-2">
-                                        <label for="guest_dietery"><strong>Dietary Requirements</strong></label>
-                                        <input class="text-input input" type="text" id="guest_dietery" name="guest_dietery" placeholder="Your dietary requirements..." value="<?= $user_details_result['guest_dietery'];?>">
-                                    </div>
+                                <div class="form-input-wrapper my-2">
+                                    <label for="guest_postcode"><strong>Postcode</strong></label>
+                                    <input class="text-input input" type="text" id="guest_postcode" name="guest_postcode" placeholder="Postcode" value="<?= $user_details_result['guest_postcode']; ?>">
+                                </div>
+                                <div class="form-input-wrapper my-2">
+                                    <label for="guest_dietery"><strong>Dietary Requirements</strong></label>
+                                    <input class="text-input input" type="text" id="guest_dietery" name="guest_dietery" placeholder="Your dietary requirements..." value="<?= $user_details_result['guest_dietery']; ?>">
+                                </div>
 
                                 <div class="button-section my-3">
                                     <button class="btn-primary form-controls-btn loading-btn" type="submit">Save Changes <img id="loading-icon" class="loading-icon d-none" src="./assets/img/icons/loading.svg" alt=""></button>
@@ -141,8 +141,8 @@ $user_id = $_SESSION['user_id'];
         $("#edit_profile").submit(function(event) {
             event.preventDefault();
             //declare form variables and collect GET request information
-            guest_id = '<?php echo $user_details_result['guest_id'];?>';
-            user_id = '<?php echo $user_details_result['user_id'];?>';
+            guest_id = '<?php echo $user_details_result['guest_id']; ?>';
+            user_id = '<?php echo $user_details_result['user_id']; ?>';
             var formData = new FormData($("#edit_profile").get(0));
             formData.append("action", "edit");
             formData.append("guest_id", guest_id);
@@ -165,7 +165,7 @@ $user_id = $_SESSION['user_id'];
             });
         });
     </script>
-   
+
 
 </body>
 
